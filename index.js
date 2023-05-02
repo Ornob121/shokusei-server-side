94150b10cd20ab9ff27302084675af129b45e972
Ornob121 <shadmantahmid121@gmail.com>
2023-05-02 19:17:28 +0600

    first recipe added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const chefs = require("./chefs.json");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("This server is running");
